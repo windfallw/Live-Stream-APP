06751146968849518f6ee14c044f3e81a3ebb0da
windfallw <51202308+windfallw@users.noreply.github.com>
2020-12-21 19:24:08 +0800

    record

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -43,17 +43,20 @@
             <v-list-item-title>{{ item.title }}</v-list-item-title>
           </v-list-item-content>
         </v-list-item>
+        <h1>{{ items[0].title }}</h1>
+        <h1>{{ drawer }}</h1>
       </v-list>
 
     </v-navigation-drawer>
 
     <v-app-bar
-        absolute
         color="#fcb69f"
         dark
+        prominent
         shrink-on-scroll
+        fade-img-on-scroll
         src="https://picsum.photos/1920/1080?random"
-        scroll-target="#scrolling-techniques-2"
+        scroll-threshold="200"
         app
     >
 
@@ -81,7 +84,6 @@
       <v-btn icon>
         <v-icon>mdi-dots-vertical</v-icon>
       </v-btn>
-
     </v-app-bar>
 
   </v-card>
@@ -91,19 +93,41 @@
 <script>
 export default {
   name: "navbar",
-  data() {
-    return {
-      items: [
-        {title: '登录', icon: 'mdi-login'},
-        {title: '仪表盘', icon: 'mdi-view-dashboard'},
-        {title: '实时直播', icon: 'mdi-video-wireless-outline'},
-        {title: '相机', icon: 'mdi-camera'},
-        {title: '退出', icon: 'mdi-logout'},
-      ],
-      drawer: null
+  data: () => ({
+    items: [
+      {title: '登录', icon: 'mdi-login'},
+      {title: '仪表盘', icon: 'mdi-view-dashboard'},
+      {title: '实时直播', icon: 'mdi-video-wireless-outline'},
+      {title: '相机', icon: 'mdi-camera'},
+      {title: '退出', icon: 'mdi-logout'},
+    ],
+    drawer: null,
+    swipeDirection: 'None',
+  }),
+  methods: {
+    swipe(direction)
+    {
+      this.swipeDirection = direction
+      if (direction === 'Right')
+      {
+        this.drawer = null
+        console.log(this.drawer)
+        console.log(this.swipeDirection)
+      }
+      else if (direction === 'Left')
+      {
+        this.drawer = false
+        console.log(this.drawer)
+        console.log(this.swipeDirection)
+      }
     }
   },
+  mounted: function ()
+  {
+
+  },
 }
+
 </script>
 
 <style scoped>
--- src/views/home.vue
+++ src/views/home.vue
@@ -1,24 +1,48 @@
 <template>
   <v-app id="inspire">
 
-    <navbar/>
-
+    <navbar ref="navbar"></navbar>
+<!--    v-touch="{-->
+<!--    left: () =>  this.$refs.navbar.swipe('Left'),-->
+<!--    right: () =>  this.$refs.navbar.swipe('Right'),-->
+<!--    up: () =>  this.$refs.navbar.swipe('Up'),-->
+<!--    down: () =>  this.$refs.navbar.swipe('Down')-->
+<!--    }"-->
     <v-main>
       <contents/>
+
+      <h1>{{drawer}}</h1>
+      <h1>123</h1>
+
     </v-main>
+
+    <footers/>
 
   </v-app>
 </template>
 
 <script>
-import navbar from "@/components/navbar";
 import contents from '@/components/contents';
+import footers from "@/components/footers";
+import navbar from "@/components/navbar";
 
 export default {
   name: 'home',
+  props:{
+    drawer:navbar.data().drawer
+  },
+  data:()=>({
+
+  }),
   components: {
     navbar,
     contents,
+    footers
+  },
+  methods: {},
+  mounted: function ()
+  {
+
   },
 };
 
