47c27ccac6dc2a4c375ecef436811903ea10d718
windfallw <51202308+windfallw@users.noreply.github.com>
2020-12-23 00:23:51 +0800

    ...

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,27 +1,103 @@
 <template>
-  <v-container>
-    <v-img
-        :src="require('../assets/img/logo.svg')"
-        class="my-3"
-        contain
-        height="200"
-    />
-    <v-img
-        :src="require('../assets/img/logo.png')"
-        class="my-3"
-        contain
-        height="200"
-    />
-  </v-container>
+
+  <v-row justify="center">
+    <v-dialog
+        v-model="dialog"
+        persistent
+        max-width="600px"
+    >
+
+      <template v-slot:activator="{ on, attrs }">
+        <v-btn
+            color="primary"
+            dark
+            v-bind="attrs"
+            v-on="on"
+        >
+          Open Dialog
+        </v-btn>
+      </template>
+
+      <v-card>
+        <v-card-title>
+          <span class="headline">User Profile</span>
+        </v-card-title>
+        <v-card-text>
+          <v-container>
+            <v-row>
+
+              <v-col
+                  cols="12"
+                  sm="6"
+                  md="4"
+              >
+                <v-text-field
+                    label="Legal middle name"
+                    hint="example of helper text only on focus"
+                ></v-text-field>
+              </v-col>
+              <v-col
+                  cols="12"
+                  sm="6"
+                  md="4"
+              >
+                <v-text-field
+                    label="Legal last name*"
+                    hint="example of persistent helper text"
+                    persistent-hint
+                    required
+                ></v-text-field>
+              </v-col>
+              <v-col cols="12">
+                <v-text-field
+                    label="Email*"
+                    required
+                ></v-text-field>
+              </v-col>
+              <v-col cols="12">
+                <v-text-field
+                    label="Password*"
+                    type="password"
+                    required
+                ></v-text-field>
+              </v-col>
+
+
+            </v-row>
+          </v-container>
+          <small>*indicates required field</small>
+        </v-card-text>
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn
+              color="blue darken-1"
+              text
+              @click="dialog = false"
+          >
+            Close
+          </v-btn>
+          <v-btn
+              color="blue darken-1"
+              text
+              @click="dialog = false"
+          >
+            Save
+          </v-btn>
+        </v-card-actions>
+
+      </v-card>
+
+    </v-dialog>
+  </v-row>
 
 </template>
 
 <script>
 export default {
-  name: 'login',
-
-  data: () => ({}),
-
-  methods: {},
+  data: () => ({
+    dialog: false,
+  }),
 }
+
 </script>
--- src/views/home.vue
+++ src/views/home.vue
@@ -5,7 +5,7 @@
 
       <v-list-item>
 
-        <v-list-item-avatar color="accent">
+        <v-list-item-avatar color="accent" @click="dialog = true">
           <span class="white--text headline">xx</span>
         </v-list-item-avatar>
 
@@ -25,6 +25,7 @@
       <v-divider></v-divider>
 
       <v-list dense nav>
+
         <v-list-item>
           <v-switch
               v-model="$vuetify.theme.dark"
@@ -33,15 +34,28 @@
           ></v-switch>
         </v-list-item>
 
-        <v-list-item v-for="item in items" :key="item.title" link>
+        <v-list-item @click="dialog=true" link>
+          <v-list-item-icon>
+            <v-icon>mdi-login</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title>登录</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+
+        <v-list-item
+            v-for="item in items"
+            :key="item.title"
+            link
+        >
           <v-list-item-icon>
             <v-icon>{{ item.icon }}</v-icon>
           </v-list-item-icon>
-
           <v-list-item-content>
             <v-list-item-title>{{ item.title }}</v-list-item-title>
           </v-list-item-content>
         </v-list-item>
+
       </v-list>
 
     </v-navigation-drawer>
@@ -83,15 +97,13 @@
       </v-btn>
     </v-app-bar>
 
-    <v-main>
-
-      <login/>
-
+    <v-main class="d-flex align-content-center flex-wrap-reverse">
+      <!--      <login/>-->
     </v-main>
 
     <v-footer padless>
       <v-col
-          class="primary lighten-2 py-4 text-center white--text"
+          class="lighten-2 text-center py-0"
           cols="12"
       >
         {{ new Date().getFullYear() }} — <strong>windfallw</strong>
@@ -101,22 +113,95 @@
       </v-col>
     </v-footer>
 
+    <v-dialog
+        v-model="dialog"
+        persistent
+        max-width="600px"
+    >
+      <v-card>
+        <v-card-title>
+          <span class="headline">User Profile</span>
+        </v-card-title>
+        <v-card-text>
+          <v-container>
+            <v-row>
+
+              <v-col
+                  cols="12"
+                  sm="6"
+                  md="4"
+              >
+                <v-text-field
+                    label="Legal middle name"
+                    hint="example of helper text only on focus"
+                ></v-text-field>
+              </v-col>
+              <v-col
+                  cols="12"
+                  sm="6"
+                  md="4"
+              >
+                <v-text-field
+                    label="Legal last name*"
+                    hint="example of persistent helper text"
+                    persistent-hint
+                    required
+                ></v-text-field>
+              </v-col>
+              <v-col cols="12">
+                <v-text-field
+                    label="Email*"
+                    required
+                ></v-text-field>
+              </v-col>
+              <v-col cols="12">
+                <v-text-field
+                    label="Password*"
+                    type="password"
+                    required
+                ></v-text-field>
+              </v-col>
+            </v-row>
+          </v-container>
+          <small>*indicates required field</small>
+        </v-card-text>
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn
+              color="blue darken-1"
+              text
+              @click="dialog = false"
+          >
+            Close
+          </v-btn>
+          <v-btn
+              color="blue darken-1"
+              text
+              @click="dialog = false"
+          >
+            Save
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+
   </v-app>
 </template>
 
 <script>
-import login from '@/components/login';
+// import login from '@/components/login';
+// import axios from 'axios'
 
 export default {
   name: 'home',
 
   data: () => ({
     items: [
-      {title: '登录', icon: 'mdi-login'},
-      {title: '仪表盘', icon: 'mdi-view-dashboard'},
-      {title: '实时直播', icon: 'mdi-video-wireless-outline'},
-      {title: '相机', icon: 'mdi-camera'},
-      {title: '退出', icon: 'mdi-logout'},
+      {title: '仪表盘', icon: 'mdi-view-dashboard', click: ''},
+      {title: '实时直播', icon: 'mdi-video-wireless-outline', click: ''},
+      {title: '相机', icon: 'mdi-camera', click: ''},
+      {title: '退出', icon: 'mdi-logout', click: ''},
     ],
     icons: [
       'mdi-facebook',
@@ -125,10 +210,11 @@ export default {
       'mdi-instagram',
     ],
     drawer: null,
+    dialog: false,
   }),
 
   components: {
-    login,
+    // login,
   },
 
   methods: {},
